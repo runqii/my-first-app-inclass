2016f46495a61ddd72cf9514a7c9f1bf4505fed1
runqii <148026580+runqii@users.noreply.github.com>
2023-11-08 18:44:19 -0500

    work in proegress

--- app/email_service.py
+++ app/email_service.py
@@ -1,4 +1,3 @@
-
 import os
 
 from dotenv import load_dotenv
@@ -32,16 +31,14 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
         print(response.status_code) #> 202 indicates SUCCESS
         print(response.body)
         print(response.headers)
+        return response.status_code
 
     except Exception as err:
         print(type(err))
         print(err)
+        return None
 
 
-
-
-user_address = input("Please enter your email address: ")
-# ...
 
 if __name__ == "__main__":
 
@@ -51,29 +48,21 @@ if __name__ == "__main__":
 
 
     my_content = """
-        ... 
+
+        <img
+            src="https://img.freepik.com/free-vector/flat-ice-cream-collection_23-2148982427.jpg"
+            alt="image of an ice cream"
+            height=100
+        >
+
+        <h1>Ice Cream Shop Menu</h1>
+
+        <p>Most popular flavors:</p>
+
+        <ul>
+            <li>Vanilla Bean </li>
+            <li>Choc </li>
+            <li>Strawberry</li>
+        </ul>
     """
     send_email(html_content=my_content, recipient_address=user_address)
-    
-
-my_content = """
-
-    <img
-        src="https://img.freepik.com/free-vector/flat-ice-cream-collection_23-2148982427.jpg"
-        alt="image of an ice cream"
-        height=100
-    >
-
-    <h1>Ice Cream Shop Menu</h1>
-
-    <p>Most popular flavors:</p>
-
-    <ul>
-        <li>Vanilla Bean </li>
-        <li>Choc </li>
-        <li>Strawberry</li>
-    </ul>
-"""
-send_email(html_content=my_content, recipient_address=user_address)
-
-
